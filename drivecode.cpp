1f71b9395f51bebe2b61b458338465cb9c8d8217
Dan K <kuchandw@gmail.com>
2020-05-17 09:16:58 -0700

    Update drivecode.cpp

--- drivecode.cpp
+++ drivecode.cpp
@@ -1,10 +1,26 @@
 2021 Robotics Drive PseudoCode
 
-
-
-
+int curLR
+int curUD
+int prevLR
+int prevUD
+int deltaLR
+int deltaUD
+int slewLim
+maxSlewRate = 1000 //percentage points/PseudoCode
+int triggerslew
 get leftstick
 get rightstick
+int upperThrottleLimit = 100;
+int lowerThrottleLimit = 100;
+//GETCOMMANDS SEC --------------------------------------
+get overall command mode from master loop
+
+case 1: driver input command  //Left stick up down is for gross throttle commands ||  LR is for turning modifier
+case 2: target camera input commands  //left stick controls throttle position || LR is camera offcenter commands LR
+case 3: ball camera input commands  //left stick controls throttle position || LR is camera offcenter commands LR
+
+
 
 control function -----------------------------
 37X^2*logX
@@ -21,4 +37,9 @@ elif throttlecommand<lowerThrottleLimit)
 }
 
 modifiers -----------------------------------
-maxSlewRate = 1000 //percentage points/PseudoCode
+
+SLEWLIMITING ------------------------------
+if deltaLR >=slewLim)
+{
+    throttlecommand = upperThrottleLimit
+}
